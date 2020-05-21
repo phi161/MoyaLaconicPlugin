179ccb869a24237a052a8d61709251895a435003
phi161 <phi@magneticpull.org>
2020-05-21 22:22:12 +0300

    Set default identifiers for the plugin itself, request and response success/failure

--- MoyaLaconicPlugin/Classes/LaconicPlugin.swift
+++ MoyaLaconicPlugin/Classes/LaconicPlugin.swift
@@ -8,16 +8,41 @@
 
 import Moya
 
-public class LaconicPlugin {
+public struct LaconicPlugin {
 
-    public init() {}
+    public var pluginIdentifier: String
+    public var requestIdentifier: String
+    public var responseIdentifier: String
+    public var successIdentifier: String
+    public var failureIdentifier: String
+
+    public init(pluginIdentifier: String = "💭",
+                requestIdentifier: String = "🔜",
+                responseIdentifier: String = "🔙",
+                successIdentifier: String = "✅",
+                failureIdentifier: String = "❌") {
+        self.pluginIdentifier = pluginIdentifier
+        self.requestIdentifier = requestIdentifier
+        self.responseIdentifier = responseIdentifier
+        self.successIdentifier = successIdentifier
+        self.failureIdentifier = failureIdentifier
+    }
 
 }
 
 extension LaconicPlugin: PluginType {
 
     public func willSend(_ request: RequestType, target: TargetType) {
-        print("will send: \(target.path)")
+        print("\(pluginIdentifier)\(requestIdentifier)")
+    }
+
+    public func didReceive(_ result: Result<Response, MoyaError>, target: TargetType) {
+        switch result {
+        case .success(let response):
+            print("\(pluginIdentifier)\(responseIdentifier)\(successIdentifier) \(response.statusCode)")
+        case .failure(let error):
+            print("\(pluginIdentifier)\(responseIdentifier)\(failureIdentifier) \(error.localizedDescription)")
+        }
     }
 
 }
