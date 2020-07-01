c7fc1774f2e9a778f17ef5d739a046ce8cfd2ba4
phi161 <phi@magneticpull.org>
2020-07-01 23:27:31 +0300

    Add a protocol to offer the chance for targets to print extra data for each endpoint

---
+++ Laconic.swift
@@ -0,0 +1,16 @@
+//
+//  Laconic.swift
+//  MoyaLaconicPlugin
+//
+//  Created by phi161 on 1/7/20.
+//  Copyright (c) 2020 phi161. All rights reserved.
+//
+
+import Foundation
+
+public protocol Laconic {
+
+    var primaryIdentifier: String { get }
+    var secondaryIdentifier: String { get }
+
+}
